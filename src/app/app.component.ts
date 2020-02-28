fc65e13dcb338be2e6b7ab9669fae526892850be
百科おじさん <tftf161107@gmail.com>
2020-02-28 21:40:29 +0900

    [add] welcomeAnimation!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Animate } from './class/animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nijiri';
+
+  animate = new Animate;
+  animation = '';
+
+  ngOnInit(): void {
+    this.randomAnimation()
+  }
+
+  randomAnimation(): void {
+    setInterval(() => {
+      const random = this.animate.random();
+
+      this.animation = random.in;
+      setTimeout(() => {
+        this.animation = random.middle;
+        setTimeout(() => {
+          this.animation = random.out;
+        }, 2000);
+      }, 2000);
+    }, 6000);
+  }
 }
---
+++ src/app/class/animate.spec.ts
@@ -0,0 +1,7 @@
+import { Animate } from './animate';
+
+describe('Animate', () => {
+  it('should create an instance', () => {
+    expect(new Animate()).toBeTruthy();
+  });
+});
