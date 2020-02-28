a320ba2c7937f78072b2a1e86a89f97b59889601
百科おじさん <tftf161107@gmail.com>
2020-02-29 05:51:58 +0900

    [add] CoreModule ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
