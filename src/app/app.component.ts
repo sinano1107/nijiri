015c9d37500a4b479c4c3ba7bef6a10a271e71c8
百科おじさん <tftf161107@gmail.com>
2020-03-03 22:36:15 +0900

    [add] 仮パスワードシステムを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit {
   animate = new Animate;
   animation = '';
   languageType = true;
+  isPasswordPage = true;
+  password = '';
 
   ngOnInit(): void {
     this.randomAnimation()
@@ -30,5 +32,15 @@ export class AppComponent implements OnInit {
         }, 2000);
       }, 2000);
     }, 6000);
+  }
+
+  pass() {
+    const now = new Date()
+    const TRUE_PASS = `${now.getMonth()+1}${now.getDate()}${now.getHours()}${now.getMinutes()}`;
+    if (TRUE_PASS === this.password) {
+      this.isPasswordPage = false;
+    } else {
+      alert('パスワードが違います');
+    }
   }
 }
